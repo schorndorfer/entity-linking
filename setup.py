05d3d5c2ad01e2ddd9a3a2bb9e20463837992fe1
schorndorfer <will.k.t@gmail.com>
2022-10-12 15:58:22 -0500

    updates

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="entity-linking",
+    version="0.1.0",
+    author="Will Thompson",
+    author_email="will.k.t@gmail.com",
+    packages=find_packages(include=["elinker", "elinker.*"]),
+    package_data={"elinker": ["data/*"]},
+    include_package_data=True,
+    install_requires=[
+        "spacy",
+        "pandas",
+        "seaborn",
+        "transformers",
+        "datasets",
+    ],
+    # entry_points={"elinker": ["elinker = elinker.cli"]},
+    extras_require={
+        "interactive": ["jupyterlab"],
+        "dev": ["black", "pyment", "twine", "tox", "bumpversion", "flake8"],
+        "test": ["pytest"],
+    },
+    python_requires=">=3.9.0",
+)
