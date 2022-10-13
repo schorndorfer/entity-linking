69558eff4a212382c819d2ba131663c91f82231a
schorndorfer <will.k.t@gmail.com>
2022-10-12 22:14:30 -0500

    updates

---
+++ elinker/__init__.py
@@ -0,0 +1,2 @@
+from elinker import datasets  # noqa: E402, F401
+from elinker import cli  # noqa: E402, F401
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,9 +1,13 @@
+import os
 from pathlib import Path
 from elinker import datasets as D
 
 
+dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
+
 def test_med_mentions():
-    doc_data, mention_data = D.med_mentions(Path("../data/medmentions/full/corpus_pubtator.txt"))
+    doc_data, mention_data = D.med_mentions(dir_path/"../data/medmentions/full/corpus_pubtator.txt")
     assert doc_data.shape == (4392, 3)
     assert mention_data.shape == (352496, 6)
     assert mention_data['pmid'].nunique() == doc_data['pmid'].nunique()
