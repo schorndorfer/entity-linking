908b70a0db5c90bc5f5d8fe26cc2cf1d17c78efc
schorndorfer <will.k.t@gmail.com>
2022-10-12 22:02:53 -0500

    updates

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -4,5 +4,7 @@ from elinker import datasets as D
 
 def test_med_mentions():
     doc_data, mention_data = D.med_mentions(Path("../data/medmentions/full/corpus_pubtator.txt"))
-    print(doc_data.head())
-    print(mention_data.head())
+    assert doc_data.shape == (4392, 3)
+    assert mention_data.shape == (352496, 6)
+    assert mention_data['pmid'].nunique() == doc_data['pmid'].nunique()
+    assert len(set(mention_data['pmid'])) == 4392
